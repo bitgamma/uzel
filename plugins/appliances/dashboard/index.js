8dfddb9ca9d38ef6f9c324ffb6f926b9b52cfb9f
Michele Balistreri <michele@briksoftware.com>
2014-04-14 12:54:42 +0200

    dashboard

--- plugins/appliances/dashboard/index.js
+++ plugins/appliances/dashboard/index.js
@@ -19,30 +19,6 @@ exports.createAppliance = function(applianceManager) {
   return app;
 }
 
-function index(req, res, next) {
-  //now show all thermometers...
-  var devices = devMgr.getDevicesByType(devMgr.DeviceType.TEMPERATURE_SENSOR);
-  var processed = 0;
-  var results = "";
-    
-  if (devices.length == 0) {
-    res.send("No devices");
-  }
-  
-  devices.forEach(function(device) {
-    devMgr.getData(device, function(command, responseTLV) {
-      
-      var thermoData = responseTLV.getFirstChild(0xA1);
-    
-      var temperature = thermoData.getFirstChild(0x82).getIntValue() / thermoData.getFirstChild(0x81).getUIntValue();
-      var unit = thermoData.getFirstChild(0x80).getUIntValue();
-      
-      results = results + temperature + " " + units[unit] + "<br>";
-      processed++;
-      
-      if (processed == devices.length) {
-        res.send(results);
-      }
-    });
-  });
+function index(req, res) {
+  res.render('index.jade', { });
 }

c421718b21d69796d1d5f7019960a00252417b6a
Michele Balistreri <michele@briksoftware.com>
2014-04-15 23:31:51 +0200

    made device-adding form work

--- plugins/appliances/dashboard/index.js
+++ plugins/appliances/dashboard/index.js
@@ -3,19 +3,29 @@ var app;
 var appMgr;
 var devMgr;
 
-var monitoredDevices = [ { "name": "Foo Thermometer", "value": "20 C" }];
+var monitoredDevices = [];
+var supportedTypes;
 
 exports.createAppliance = function(applianceManager) {
   if (!app) {
-    app = express();
-    app.use(express.static(__dirname + '/public'));
-    app.set('views', __dirname + '/views');
-    
     appMgr = applianceManager;
     devMgr = appMgr.deviceManager;
+    
+    app = express();
+    app.set('views', __dirname + '/views');
+    app.use(express.static(__dirname + '/public'));
+    
     app.get('/', index);
+    app.get('/getDevicesByType', getDevicesByType);
+    app.get('/getSupportedTypes', getSupportedTypes);
     appMgr.io.of('/dashboard').on('connection', handleDashboardSocketConnection);
     
+    //TODO: fill this array through plugins
+    supportedTypes = [
+      { 'name': 'All', 'value': [ devMgr.DeviceType.TEMPERATURE_SENSOR, devMgr.DeviceType.MOISTURE_SENSOR ] },
+      { 'name': 'Temperature Sensor', 'value': [ devMgr.DeviceType.TEMPERATURE_SENSOR ] },
+      { 'name': 'Moisture Sensor', 'value': [ devMgr.DeviceType.MOISTURE_SENSOR ] },
+    ];
   }
   
   return app;
@@ -25,6 +35,31 @@ function index(req, res) {
   res.render('index.jade', { });
 }
 
+function getDevicesByType(req, res) {
+  res.json(devMgr.getDevicesByType(req.query.types).filter(function(element) { 
+    return monitoredDevices.indexOf(element) == -1;
+  }).map(deviceDescriptor));
+}
+
+function getSupportedTypes(req, res) {
+  res.json(supportedTypes);
+}
+
+
 function handleDashboardSocketConnection(socket) {
   socket.emit('init', monitoredDevices);
+  socket.on('addMonitoredDevice', addMonitoredDevice.bind(null, socket));
+}
+
+function addMonitoredDevice(socket, deviceID) {
+  var device = devMgr.getDeviceByFullID(deviceID);
+  
+  if (device) {
+    monitoredDevices.push(device);
+    appMgr.io.of('/dashboard').emit('addedDevice', device);
+  }
+}
+
+function deviceDescriptor(device) {
+  return { 'id': device.getFullID(), 'name': device.deviceInfo.name };
 }
--- plugins/appliances/dashboard/public/js/dashboard.js
+++ plugins/appliances/dashboard/public/js/dashboard.js
@@ -1,6 +1,6 @@
 var dashboard = angular.module('dashboard', [
   'btford.socket-io',
-  'ngAnimate'
+  'ngAnimate',
 ]).factory('dashboardSocket', function (socketFactory) {
   var dashboardIOSocket = io.connect('/dashboard');
 
@@ -11,10 +11,43 @@ var dashboard = angular.module('dashboard', [
   return dashboardSocket;
 });
  
-dashboard.controller('DashboardController', function ($scope, dashboardSocket) { 
-  $scope.addingDevice = false;
+dashboard.controller('DashboardController', function ($scope, $http, dashboardSocket) {     
+  $scope.deviceTypes = [];
+
+  $scope.resetAddForm = function() {
+    $scope.deviceType = null;
+  
+    $scope.availableDevices = [];
+    $scope.deviceToAdd = null;  
+    $scope.addingDevice = false;  
+  }
+  
+  $scope.resetAddForm();
    
   dashboardSocket.on('init', function(monitoredDevices) {
     $scope.monitoredDevices = monitoredDevices; 
   });
+  
+  dashboardSocket.on('addedDevice', function(device) {
+    $scope.monitoredDevices.push(device); 
+  });
+  
+  $http.get('/dashboard/getSupportedTypes').success(function(data) {
+    $scope.deviceTypes = data;
+  });
+  
+  $scope.selectDeviceType = function() {
+    if ($scope.deviceType) {
+      $http.get('/dashboard/getDevicesByType', { params: { types: $scope.deviceType.value } }).success(function(data) {
+        $scope.availableDevices = data;
+      });      
+    }
+  };
+  
+  $scope.addDevice = function() {
+    if ($scope.deviceToAdd) {
+      dashboardSocket.emit('addMonitoredDevice', $scope.deviceToAdd.id);
+      $scope.resetAddForm();
+    }
+  };
 });

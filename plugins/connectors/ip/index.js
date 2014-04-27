2357ae56163e4091452845ad3901ed40f51db7f0
Michele Balistreri <michele@briksoftware.com>
2014-04-27 11:57:15 +0200

    added basic OSNP with MRF24J40 connector

--- plugins/connectors/ip/index.js
+++ plugins/connectors/ip/index.js
@@ -5,16 +5,18 @@ exports.start = function() {
 }
 
 exports.pair = function(device, pairingData, cb) {
+  //maybe establish connection here
   cb(device);
 }
 
 exports.unpair = function(device, cb) {
+  //drop connection here
   cb(device);
 }
 
 exports.discoverDevices = function(cb) {
   //substitute with real code, maybe db, maybe SSDP, maybe both?
-  var discoveredDevices = [ new IPProtocolInfo('127.0.0.1', 4000) ];
+  var discoveredDevices = [];
 
   for(var i = 0, len = discoveredDevices.length; i < len; i++) {
     var dev = discoveredDevices[i];
@@ -23,6 +25,7 @@ exports.discoverDevices = function(cb) {
 }
 
 exports.send = function(device, data, cb) {  
+  //the connection should probably be already open
   var client = net.connect(device.protocolInfo.port, device.protocolInfo.address, function() {
     client.on('data', function(responseData) {
       client.end();
